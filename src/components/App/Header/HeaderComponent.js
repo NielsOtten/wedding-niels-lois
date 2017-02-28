fab9a3c97041e278f5e56b59f81db5cb7c558331
Niels <niels@burst-digital.com>
2017-02-28 14:23:04 +0100

    Add header component

--- src/components/App/Header/HeaderComponent.js
+++ src/components/App/Header/HeaderComponent.js
@@ -1,33 +1,53 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import styles from './styles.scss';
 
-const HeaderComponent = () => {
-  return (
-    <header>
-      <div className="title">
-        Loïs & Niels
-      </div>
-      <div className="nav-trigger">
-        <span/>
-      </div>
-      <div className="main-menu">
-        <ul>
-          <li className="menu-link">
-            <Link to="/home">HOME</Link>
-          </li>
-          <li className="menu-link">
-            <Link to="/rsvp">RSVP</Link>
-          </li>
-          <li className="menu-link">
-            <Link to="/home">STORY</Link>
-          </li>
-          <li className="menu-link">
-            <Link to="/home">TRAVEL</Link>
-          </li>
-        </ul>
-      </div>
-    </header>
-  );
-};
+class HeaderComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  render() {
+    return (
+      <header>
+        <div className="title">
+          Loïs & Niels
+        </div>
+        <div className={styles['nav-trigger']} onClick={() => this.openMenu()}>
+          Menu
+          <span/>
+        </div>
+        <div className={styles['main-menu'] + ' ' + (this.state.open ? styles['active'] : '')}>
+          <ul>
+            <li className={styles['menu-link']}>
+              <Link to="/home">HOME</Link>
+            </li>
+            <li className={styles['menu-link']}>
+              <Link to="/rsvp">RSVP</Link>
+            </li>
+            <li className={styles['menu-link']}>
+              <Link to="/home">STORY</Link>
+            </li>
+            <li className={styles['menu-link']}>
+              <Link to="/home">TRAVEL</Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+    );
+  }
+
+  openMenu() {
+    if (this.state.open) {
+      this.setState({open: false});
+    } else {
+      this.setState({open: true});
+    }
+  }
+}
 
 export default HeaderComponent;
