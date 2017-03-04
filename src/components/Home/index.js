93c8f55e0daf0aadd9bd9eef4f2a4d3069516b59
Niels <niels@burst-digital.com>
2017-03-04 17:14:21 +0100

    Add colomn to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,8 +9,9 @@ class Home extends React.Component {
 
   render() {
     return (
-      <section>
-        Home
+      <section className={styles['colomn']}>
+        <p className={styles['col-6']}>Wij hebben onze liefde & geluk in elkaar gevonden en zijn daarom klaar om samen deze wereld te ontdekken. Voordat we dit avontuur beginnen willen wij elkaar trouw beloven en delen wij dus met heel veel vreugde mee dat wij op 17-8-17 in het huwelijksbootje zullen stappen!</p>
+        <p className={styles['col-6']}>Wij zouden graag met jullie hier een groot feest van maken, so save the date! Uitnodigingen volgen en via deze website zullen we t.z.t. meer informatie plaatsen voor onze grote dag.</p>
       </section>
     );
   }
