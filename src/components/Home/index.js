13a724214ec327f26cbae7342c189b0b18b8b731
Niels <niels@burst-digital.com>
2017-05-04 15:31:12 +0200

    Remove text from homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,12 +9,6 @@ class Home extends React.Component {
   }
 
   render() {
-    const text = 'Wij hebben onze liefde & geluk in elkaar gevonden en zijn daarom klaar om samen deze wereld te ' +
-      'ontdekken. Voordat we dit avontuur beginnen willen wij elkaar trouw beloven en delen wij dus met heel veel ' +
-      'vreugde mee dat wij op 17-8-17 in het huwelijksbootje zullen stappen! ' +
-      'Wij zouden graag met jullie hier een groot feest van maken, so save the date! Uitnodigingen volgen en via deze' +
-      ' website zullen we t.z.t. meer informatie plaatsen voor onze grote dag.';
-
     return (
       <div>
         <section className={styles['container']}>
