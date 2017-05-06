fa0edbc4139f86c093e28dd8c0355f23dc20d90d
Niels <niels@burst-digital.com>
2017-05-06 19:26:40 +0200

    Feedback styling

--- src/components/Home/Plan/index.js
+++ src/components/Home/Plan/index.js
@@ -42,21 +42,36 @@ class Plan extends React.Component {
             margin: '0 0 0 -' + this.state.margin + 'px',
             width: this.state.width
           }}/>
-          <h2>Planning</h2>
-          <div className={styles['time']}>
-            <img className={styles['icon']} src={require('../../../images/engagement-ring.svg')} alt="Wedding ring"/>
-            <p>Kerkdienst</p>
-            <h6>19:00</h6>
+          <div className={styles['information']}>
+            <div className={[styles['location'], styles['info']].join('')}>
+              <h3>Location</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum cumque deserunt doloremque earum, et ex exercitationem explicabo, facilis harum itaque laborum laudantium neque quibusdam quos rem repellendus repudiandae, voluptatem.</p>
+            </div>
+            <div className={[styles['location'], styles['info']].join('')}>
+              <h3>Location</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum cumque deserunt doloremque earum, et ex exercitationem explicabo, facilis harum itaque laborum laudantium neque quibusdam quos rem repellendus repudiandae, voluptatem.</p>
+            </div>
+            <div className={[styles['location'], styles['info']].join('')}>
+              <h3>Location</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cum cumque deserunt doloremque earum, et ex exercitationem explicabo, facilis harum itaque laborum laudantium neque quibusdam quos rem repellendus repudiandae, voluptatem.</p>
+            </div>
           </div>
-          <div className={styles['time']}>
-            <img className={styles['icon']} src={require('../../../images/chat.svg')} alt="Praten"/>
-            <p>Receptie</p>
-            <h6>20:30</h6>
-          </div>
-          <div className={styles['time']}>
-            <img className={styles['icon']} src={require('../../../images/chat.svg')} alt="Feest"/>
-            <p>Feest</p>
-            <h6>21:30</h6>
+          <div className={styles['planning']}>
+            <div className={styles['time']}>
+              <img className={styles['icon']} src={require('../../../images/church.svg')} alt="Kerk"/>
+              <p>Kerkdienst</p>
+              <h6>19:00</h6>
+            </div>
+            <div className={styles['time']}>
+              <img className={styles['icon']} src={require('../../../images/chat.svg')} alt="Praten"/>
+              <p>Receptie</p>
+              <h6>20:30</h6>
+            </div>
+            <div className={styles['time']}>
+              <img className={styles['icon']} src={require('../../../images/romantic-music.svg')} alt="Feest"/>
+              <p>Feest</p>
+              <h6>21:30</h6>
+            </div>
           </div>
         </div>
       </section>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -18,13 +18,13 @@ class Home extends React.Component {
           <div className={styles['text']}>
             <h2>Verkering</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam consectetur dicta dolor eos in nesciunt possimus quod repellendus, repudiandae tempora veniam veritatis! Iusto provident quaerat quidem sapiente veniam voluptas.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam consectetur dicta dolor eos in nesciunt possimus quod repellendus, repudiandae tempora veniam veritatis! Iusto provident quaerat quidem sapiente veniam voluptas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi culpa dolorem et ipsum iste laudantium sit vero. Aliquid, aspernatur atque aut ducimus expedita incidunt magnam minus, molestiae nisi sed voluptatibus.
             </p>
           </div>
           <div className={styles['text']}>
             <h2>Verloving</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius eligendi id illo illum impedit minus.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam consectetur dicta dolor eos in nesciunt possimus quod repellendus, repudiandae tempora veniam veritatis! Iusto provident quaerat quidem sapiente veniam voluptas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi culpa dolorem et ipsum iste laudantium sit vero. Aliquid, aspernatur atque aut ducimus expedita incidunt magnam minus, molestiae nisi sed voluptatibus.
             </p>
           </div>
         </section>
