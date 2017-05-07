b63e3663e2c1d9f6b1972f12027584f646998ea5
Niels <niels@burst-digital.com>
2017-05-07 19:24:02 +0200

    Add google analytics

--- src/server/template.js
+++ src/server/template.js
@@ -18,12 +18,20 @@ export default vo => `
     ${vo.cssBundle ? '<link rel="stylesheet" type="text/css" href="' + vo.cssBundle + '">' : ''}
     <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto|Montserrat" rel="stylesheet">
     <title>Wedding Niels & Loïs</title>
-    <script src="https://maps.googleapis.com/maps/api/js"></script>
   </head>
 
   <body>
     <div id="root" style="height:100vh"><div>${vo.root}</div></div>
     <script src="${vo.jsBundle}"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-98700191-1', 'auto');
+      ga('send', 'pageview');
+    </script>
   </body>
 
 </html>
