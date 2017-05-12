d798be90e9d4081d2f0411f1dc43d57f458b7585
Niels <niels@burst-digital.com>
2017-05-12 08:26:15 +0200

    Add og tags.

--- src/server/template.js
+++ src/server/template.js
@@ -9,15 +9,35 @@ export default vo => `
   <head>
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta charSet='utf-8' />
-    <meta httpEquiv="Content-Language" content="en" />
+    <meta httpEquiv="Content-Language" content="nl" />
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-
     <link id="favicon" rel="shortcut icon" href="${require('../images/engagement-ring.svg')}" sizes="16x16 32x32" type="image/png" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
     ${vo.cssBundle ? '<link rel="stylesheet" type="text/css" href="' + vo.cssBundle + '">' : ''}
     <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto|Montserrat" rel="stylesheet">
     <title>Wedding Niels & Loïs</title>
+    
+    <meta name="msapplication-TileColor" content="#ffffff">
+	  <meta name="msapplication-TileImage" content="/complimentos/images/icons/ms-icon-144x144.png">
+	  <meta name="theme-color" content="#ffffff">
+    
+    <meta property="og:url" content="http://nielsenlois.nl"/>
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="Wedding Niels & Loïs" />
+		<meta property="og:description" content="17 Augustus 2017 gaan Niels & Loïs trouwen! Be there!" />
+		<meta name="og:image:height" content="600">
+		<meta property="og:image" content="${require('../images/Share-image-fb.png')}" />
+
+		<meta name="twitter:card" content="photo" />
+		<meta name="twitter:site" content="@nielsenlois" />
+		<meta name="twitter:title" content="Wedding Niels & Loïs" />
+		<meta name="twitter:description" content="17 Augustus 2017 gaan Niels & Loïs trouwen! Be there!" />
+		
+		<meta name="twitter:image:width" content="1143">
+		<meta name="twitter:image:height" content="600">
+		<meta name="twitter:image" content="${require('../images/Share-image-fb.png')}" />
+		<meta name="twitter:url" content="http://nielsenlois.nl" />
   </head>
 
   <body>
