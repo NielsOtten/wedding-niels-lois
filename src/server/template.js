f3140c6795d6bba695623fbd2ad487d6fa507316
Niels <niels@burst-digital.com>
2017-06-04 15:21:15 +0200

    Add google analytics

--- src/server/template.js
+++ src/server/template.js
@@ -47,7 +47,7 @@ export default vo => `
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
     
-      ga('create', 'UA-98700191-1', 'auto');
+      ga('create', 'UA-100417048-1', 'auto');
       ga('send', 'pageview');
     </script>
   </body>
